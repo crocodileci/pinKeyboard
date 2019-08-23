107c578f22dbcf6f4f4e44241849c51c312f1f87
Grady Dun <gradydun@hitrust.com.tw>
2019-08-23 18:00:40 +0800

    在主頁面新增一個按鈕並綁定click事件處理函式

--- app/src/main/java/com/example/gradydun/myapplication/MainActivity.java
+++ app/src/main/java/com/example/gradydun/myapplication/MainActivity.java
@@ -2,12 +2,30 @@ package com.example.gradydun.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final String TAG = "MainActivity";
+    Button btn2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn2 = findViewById(R.id.button2);
+
+        btn2.setOnClickListener(btn2Listener);
     }
+
+    private Button.OnClickListener btn2Listener = new Button.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            // TODO Auto-generated method stub
+            Log.e(TAG, "button 2 clicked");
+        }
+    };
 }
