ee79d7e01d34462ec103182d740f55b5e49d7fd1
Grady Dun <gradydun@hitrust.com.tw>
2019-08-25 14:52:42 +0800

    在主程式中調用該新增之Dialog

--- app/src/main/java/com/example/gradydun/myapplication/MainActivity.java
+++ app/src/main/java/com/example/gradydun/myapplication/MainActivity.java
@@ -6,6 +6,8 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.smartcarddialog.SmartcardDialog;
+
 public class MainActivity extends AppCompatActivity {
 
     private final String TAG = "MainActivity";
@@ -26,6 +28,9 @@ public class MainActivity extends AppCompatActivity {
         public void onClick(View v) {
             // TODO Auto-generated method stub
             Log.e(TAG, "button 2 clicked");
+            SmartcardDialog dialog = new SmartcardDialog();
+            dialog.setCancelable(false);
+            dialog.show(getFragmentManager(), "Test");
         }
     };
 }
