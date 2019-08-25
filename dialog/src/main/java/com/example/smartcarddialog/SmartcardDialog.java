99d94d2df4aa792a4a65a68b35345705a952e050
Grady Dun <gradydun@hitrust.com.tw>
2019-08-25 15:53:52 +0800

    Dialog 加入檢核條件與scrollView 若layout超過可以顯示的大小時可以捲動

--- dialog/src/main/java/com/example/smartcarddialog/SmartcardDialog.java
+++ dialog/src/main/java/com/example/smartcarddialog/SmartcardDialog.java
@@ -9,6 +9,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+
+import java.lang.reflect.Field;
 
 /**
  * Created by gradydun on 2019/8/23.
@@ -19,6 +22,7 @@ public class SmartcardDialog extends DialogFragment {
     private final String TAG = "SmartcardDialog";
     Button btn_confirm;
     Button btn_cancel;
+    EditText editText_pinCode;
 
 //    @Override
 //    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
@@ -37,6 +41,8 @@ public class SmartcardDialog extends DialogFragment {
         // Inflate and set the layout for the dialog
         // Pass null as the parent view because its going in the dialog layout
 
+        editText_pinCode = view.findViewById(R.id.pincode);
+
         builder.setView(view)
                 .setTitle("查詢餘額")
                 // Add action buttons
@@ -44,14 +50,44 @@ public class SmartcardDialog extends DialogFragment {
                     @Override
                     public void onClick(DialogInterface dialog, int id) {
                         Log.e(TAG, "on click confirm handler");
+
+                        int pincode_length = editText_pinCode.getText().length();
+
+                        //如果檢核條件Pass則直接return
+                        try {
+                            if (pincode_length >= 6 && pincode_length <= 12) {
+                                //關閉Dialog
+                                Field field = dialog.getClass().getSuperclass().getDeclaredField("mShowing");
+                                field.setAccessible(true);
+                                field.set(dialog, true);
+                            } else {
+                                //不關閉Dialog
+                                Field field = dialog.getClass().getSuperclass().getDeclaredField("mShowing");
+                                field.setAccessible(true);
+                                field.set(dialog, false);
+                            }
+                        }catch (Exception e){
+                            e.printStackTrace();
+                        }
                     }
                 })
                 .setNegativeButton("取消Cancel", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
                         Log.e(TAG, "on click cancel handler");
+                        try {
+                            Field field = dialogInterface.getClass().getSuperclass().getDeclaredField("mShowing");
+                            field.setAccessible(true);
+                            field.set(dialogInterface, true);
+                        } catch (Exception e){
+                            e.printStackTrace();
+                        }
                     }
                 });
         return builder.create();
+    }
+
+    private void shuffleNumbericPad(View view){
+
     }
 }
