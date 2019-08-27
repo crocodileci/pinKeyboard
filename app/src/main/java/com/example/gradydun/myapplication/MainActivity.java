12506db1a7db45cf1140b21e281f18ee61b26ed5
Grady Dun <gradydun@hitrust.com.tw>
2019-08-27 10:40:11 +0800

    APP 增加處理dialog回傳結果的callback function

--- app/src/main/java/com/example/gradydun/myapplication/MainActivity.java
+++ app/src/main/java/com/example/gradydun/myapplication/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.gradydun.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Display;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.smartcarddialog.SmartcardDialog;
 
@@ -26,7 +29,6 @@ public class MainActivity extends AppCompatActivity {
         btn3 = findViewById(R.id.button3);
 
         btn3.setOnClickListener(btn3Listener);
-
     }
 
     private Button.OnClickListener btn2Listener = new Button.OnClickListener() {
@@ -38,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
 
             dialog.setTitle("餘額查詢");
             dialog.setDialogType(SmartcardDialog.VERIFY_PIN);
+            dialog.setOnInputCompleteListener(completeListener);
 
             //禁止點選Dailog外部或返回鍵時關閉Dialog
             dialog.setCancelable(false);
@@ -54,6 +57,7 @@ public class MainActivity extends AppCompatActivity {
 
             dialog.setTitle("變更密碼");
             dialog.setDialogType(SmartcardDialog.CHANGE_PIN);
+            dialog.setOnInputCompleteListener(completeListener);
 
             //禁止點選Dailog外部或返回鍵時關閉Dialog
             dialog.setCancelable(false);
@@ -61,4 +65,23 @@ public class MainActivity extends AppCompatActivity {
             dialog.show(getFragmentManager(), "Test");
         }
     };
+
+    private SmartcardDialog.InputCompleteListener completeListener = new SmartcardDialog.InputCompleteListener() {
+        @Override
+        public void inputComplete(boolean isCanceled, String pinCode, String pinCode_new1, String pinCode_new2) {
+            if(isCanceled){
+                showToast("User Canceled");
+            }else{
+                showToast("pinCode: " + pinCode + " pinCode_new1: " + pinCode_new1 + " pinCode_new2: " + pinCode_new2);
+            }
+        }
+    };
+
+    private void showToast(String message){
+        Display display = getWindowManager().getDefaultDisplay();
+        int height = display.getHeight();
+        Toast toast = Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG);
+        toast.setGravity(Gravity.TOP, 0, height / 6);
+        toast.show();
+    }
 }
